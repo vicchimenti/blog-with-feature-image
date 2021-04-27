a6c1f259878919ee2730a44fb262005099cf0c7f
Vic Chimenti <victor.chimenti@gmail.com>
2021-04-27 10:15:55 -0700

    cm types

--- article-content-type.js
+++ article-content-type.js
@@ -2,26 +2,20 @@
 *     @author Victor Chimenti, MSCS-SE '20
 *     @file article-content-type.js
 *
-*     This new content type is being adapted from the knowledge base
-*     content type used by IT Services and is intended to provide a
-*     searchable, sortable group of articles that can be exported to and used
-*     by any department.
-*
-*     This specific project is intended for the Career Engagement Dept,
-*     although this content type should easily be exportable to other teams.
+*     This new blog content type is derived from the Career Engagement Blog.
 *
 *     This content type will work in conjunction with the Organizer and each item
 *     will contain one searchable, article.
 *
 *     Document will write once when the page loads
 *
-*     @version 2.6
+*     @version 3.1
 */
 
 try {
     /* -- Store all the things -- */
     var articleTitle = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Title' output='normal' display_field='value' />");
-    var articleTypes = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Categories' output='normal' display_field='value' />");
+    // var articleTypes = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Categories' output='normal' display_field='value' />");
     var publishDate = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Publish Date' output='normal' display_field='value' />");
     var author = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Author' output='normal' display_field='value' />");
     var programImageMedia = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Article Image' output='normal' formatter='path/*' />");
@@ -32,10 +26,10 @@ try {
     var fullTextLink = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Name' output='fulltext' use-element='true' filename-element='Name' modifiers='striptags,htmlentities' />");
     var titleLink = "";
     var lastModified = '<div class="lastModified" style="display:inline-block">Last modified: <t4 type="meta" meta="last_modified" format="MMMM d, yyyy" /></div>';
-    var fieldKeywords = content.get("Searchable Keyword");
-    var fieldTags = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Tags' output='normal' display_field='value' />");
-    var listOfTags = "";
-    var listOfTypes = "";
+    // var fieldKeywords = content.get("Searchable Keyword");
+    // var fieldTags = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Tags' output='normal' display_field='value' />");
+    // var listOfTags = "";
+    // var listOfTypes = "";
   
   
     /* -- Prepare all the things -- */
@@ -44,30 +38,28 @@ try {
   
   
     /* parse the list of tags, add <li> tags*/
-    if (fieldTags != "") {
-      var arrayOfTags = fieldTags.split(',');
-      for (let i = 0; i < arrayOfTags.length; i++) {
-        listOfTags += '<li class="tag">' + arrayOfTags[i] + '</li>';
-      }
-      listOfTags = '<ul>' + listOfTags + '</ul>';
-    }
+    // if (fieldTags != "") {
+    //   var arrayOfTags = fieldTags.split(',');
+    //   for (let i = 0; i < arrayOfTags.length; i++) {
+    //     listOfTags += '<li class="tag">' + arrayOfTags[i] + '</li>';
+    //   }
+    //   listOfTags = '<ul>' + listOfTags + '</ul>';
+    // }
   
       /* parse the list of categories, add <li> tags*/
-    if (articleTypes != "") {
-      var arrayOfTypes = articleTypes.split(',');
-      for (let i = 0; i < arrayOfTypes.length; i++) {
-        listOfTypes += '<li class="articleType">' + arrayOfTypes[i] + '</li>';
-      }
-      listOfTypes = '<ul>' + listOfTypes + '</ul>';
-    }
+    // if (articleTypes != "") {
+    //   var arrayOfTypes = articleTypes.split(',');
+    //   for (let i = 0; i < arrayOfTypes.length; i++) {
+    //     listOfTypes += '<li class="articleType">' + arrayOfTypes[i] + '</li>';
+    //   }
+    //   listOfTypes = '<ul>' + listOfTypes + '</ul>';
+    // }
   
     /* determine which link, if any, goes in the title */
-    if (fieldSectionLink == "" && articleFullBody == "") {
+    if (articleFullBody == "") {
       titleLink = "<h4>" + articleTitle + "</h4>";
-    } else if (fieldSectionLink == "") {
-      titleLink = '<h4><a href="' + fullTextLink + '">' + articleTitle + '</a></h4>';
     } else {
-      titleLink = '<h4><a href="' + fieldSectionLink + '">' + articleTitle + '</a></h4>';
+      titleLink = '<h4><a href="' + fieldSectionLink + '" title="Read the full ' + articleTitle + '">' + articleTitle + '</a></h4>';
     }
   
     
@@ -97,24 +89,24 @@ try {
       document.write('<div class="publishDate articleDetails articleDate" style="display: none";><h5>No Date Provided</h5></div>');
     }
   
-    document.write(listOfTags);
+    // document.write(listOfTags);
   
   
     // document.write(listOfTypes);
   
     /* -- Write Program Level 1 Details --*/
-    if (listOfTypes != "") {
-      document.write('<div class="levelOne">');
-      document.write('<div class="articleDetails articleTypes"><h5>Categories: </h5><div class="articleTypes"><span>' + listOfTypes + '</span></div></div>');
-      document.write('</div>');
-    } else {
-      document.write('<div class="levelOne articleDetails articleType" style="display: none";><h5>No Category Provided</h5></div>');
-    }
+    // if (listOfTypes != "") {
+    //   document.write('<div class="levelOne">');
+    //   document.write('<div class="articleDetails articleTypes"><h5>Categories: </h5><div class="articleTypes"><span>' + listOfTypes + '</span></div></div>');
+    //   document.write('</div>');
+    // } else {
+    //   document.write('<div class="levelOne articleDetails articleType" style="display: none";><h5>No Category Provided</h5></div>');
+    // }
   
   
   
     document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, lastModified));
-    document.write('<div class="keywords" style="display:none;" aria-hidden="true">' + fieldKeywords + '</div>');
+    // document.write('<div class="keywords" style="display:none;" aria-hidden="true">' + fieldKeywords + '</div>');
     document.write('</div>'); // close summaryWrapper
   
     document.write(endingHTML);
