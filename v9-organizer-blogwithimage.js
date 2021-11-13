585e436a17f1ab187281ba532a82d1586435e015
Vic Chimenti <victor.chimenti@gmail.com>
2021-11-12 18:11:42 -0800

    article title

--- v9-organizer-blogwithimage.js
+++ v9-organizer-blogwithimage.js
@@ -40,17 +40,11 @@
       var externalLink = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='External Link' output='normal' modifiers='striptags,htmlentities' />");
       var externalLinkText = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='External Link Text' output='normal' modifiers='striptags,htmlentities' />");
       var articleFullBody = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Article Body' output='normal' display_field='value' />");
-      var fullTextLink = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Name' output='fulltext' use-element='true' filename-element='Article Title' modifiers='striptags,htmlentities' />");
+      var fullTextLink = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Article Title' output='fulltext' use-element='true' filename-element='Article Title' modifiers='striptags,htmlentities' />");
       var contentID = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='meta' meta='content_id' />");
 
       var imageID = content.get('Image').getID();
-    //   var imageName = content.get('Image').getName();
-    //   imageInfo.setInput(imageID) 
-    //   imageInfo.check();
-    //   var imageHeight = content.get('Image').getHeight();
-    //   var imageWidth = imageInfo.getWidth();
 
-    // height="' + imageHeight + '" width="' + imageWidth + '" 
 
       
       /***
